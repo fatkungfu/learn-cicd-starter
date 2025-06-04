99a76b8845bcb38954a76537657daa04e66b955c
Eric Bourcier-Walsh <fatkungfu@gmail.com>
2025-06-03 21:19:47 -0400

    Enhance JSON response handling and add ReadHeaderTimeout to HTTP server

--- json.go
+++ json.go
@@ -30,5 +30,8 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-	w.Write(dat)
+	if _, err := w.Write(dat); err != nil {
+		log.Printf("Error writing response: %s", err)
+		// At this point headers are already sent, so we can't change the status code
+	}
 }
